fd096fd2df3356e26ea2039b151e097bf9ddda34
sangram73 <pandasangram20@gmail.com>
2024-06-16 23:49:29 +0530

    adding some new method and creating data base

--- app.py
+++ app.py
@@ -2,16 +2,21 @@ from flask import Flask, render_template, redirect, url_for, request, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager, UserMixin, login_user, current_user, logout_user, login_required
+import os
 
+# Initialize the Flask app
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
+# Initialize extensions
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
 
+# User model
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
@@ -61,7 +66,8 @@ def login():
         user = User.query.filter_by(email=email).first()
         if user and bcrypt.check_password_hash(user.password, password):
             login_user(user, remember=request.form.get('remember'))
-            return redirect(url_for('home'))
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('home'))
         else:
             flash('Login Unsuccessful. Please check email and password', 'danger')
     return render_template('login.html')
@@ -76,7 +82,13 @@ def logout():
 def profile():
     return render_template('profile.html', user=current_user)
 
-# Define routes for search, request management, and notifications later
-
+# Function to create the database
+def create_database():
+    if not os.path.exists('site.db'):
+        with app.app_context():
+            db.create_all()
+            print('Database created!')
+# Main block to run the app and create the database if it doesn't exist
 if __name__ == '__main__':
+    create_database()
     app.run(debug=True)
